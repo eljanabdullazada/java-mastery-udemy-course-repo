d79af03d14023c0ab4040bc31e9b395bee3513ab
eljan_abdullazada <a3371977@gmail.com>
2026-01-14 23:34:36 +0400

    complete Java Collections, Map Interface Part2 (Lesson 15) complete

--- Java Mastery/src/collections/MapInterfaceExample.java
+++ Java Mastery/src/collections/MapInterfaceExample.java
@@ -1,10 +1,40 @@
 package collections;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MapInterfaceExample {
     public static void main(String[] args) {
+
+        //TreeMap
+        TreeMap<Integer, String> phoneCodes = new TreeMap<>();
+        phoneCodes.put(90, "Turkey");
+        phoneCodes.put(44, "United Kingdom");
+        phoneCodes.put(33, "France");
+        phoneCodes.put(49, "Germany");
+        phoneCodes.put(20, "Egypt");
+        phoneCodes.put(49, "Germany");
+        phoneCodes.put(91, "India");
+        System.out.println("phoneCodes = " + phoneCodes);
+        //System.out.println("Descending map = " + phoneCodes.descendingMap());
+        //System.out.println("Descending key set = " + phoneCodes.descendingKeySet());
+        phoneCodes.remove(44);
+        for (Map.Entry<Integer, String> phoneCodeEntry: phoneCodes.entrySet()) {
+            System.out.println(phoneCodeEntry.getKey() + " is phone code of the " + phoneCodeEntry.getValue());
+        }
+
+        /*
+        LinkedHashMap<String, String> colorsMap = new LinkedHashMap<>();
+        colorsMap.put("BMW", "Blue");
+        colorsMap.put("Opel", "Green");
+        colorsMap.put("Ferrari", "Red");
+        colorsMap.put("Ferrari", "White");
+        colorsMap.put("Mercedes", "Black");
+        System.out.println("colors Map = " + colorsMap);
+
+
         Map<String, Integer> carsMap = new HashMap<>();
 
         carsMap.put("Mercedes", 40008);
@@ -32,5 +62,19 @@ public class MapInterfaceExample {
             System.out.println("Price of " + mapEntry.getKey() + " is " + mapEntry.getValue());
         }
 
+        //HashMap
+        // The insertion order is not preserved
+        HashMap<String, Integer> agesMap = new HashMap<>();
+
+        agesMap.put("Alex", 40);
+        agesMap.put("James", 20);
+        agesMap.put("James", 30);
+        agesMap.put("Ronaldo", 35);
+        agesMap.put("Marry", 25);
+
+
+        System.out.println("agesMap = " + agesMap);
+
+        */
     }
 }
