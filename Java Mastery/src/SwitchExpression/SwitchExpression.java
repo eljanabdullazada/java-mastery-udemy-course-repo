1c9a8f18f879039339fed824e32f0b3eb454f488
eljan_abdullazada <a3371977@gmail.com>
2026-03-08 11:00:01 +0400

    Section 20: Switch Expression(Enhanced Switch Statement)

---
+++ Java Mastery/src/SwitchExpression/SwitchExpression.java
@@ -0,0 +1,49 @@
+package SwitchExpression;
+
+public class SwitchExpression {
+    public static void main(String[] args) {
+
+        int day = 3;
+        String message = "";
+
+        message = switch(day){
+            case 1, 2, 3, 4, 5 -> {
+                yield "Workday";
+            }
+            case 6, 7 ->  "Weekend";
+            default -> "Invalid day";
+        };
+        System.out.println("message: " + message);
+
+         /*
+        // Pattern matching
+        Object object = "123";
+        String message = switch (object){
+            case Integer i -> object + " is an Integer.";
+            case String s -> object + " is a String.";
+            default -> "Unknown type.";
+        };
+//        if (object instanceof Integer){
+//            message = object + " is an Integer.";
+//        } else if (object instanceof String) {
+//            message = object + " is a String.";
+//        } else{
+//            message = "Unknown type";
+//        }
+        System.out.println(message);
+
+
+        // Enum
+        Days day = Days.MONDAY;
+        String message = switch(day){
+            case MONDAY, TUESDAY, WEDNESDAY, FRIDAY -> "Weekdays";
+            case SATURDAY, SUNDAY -> "Weekend";
+            default -> "Invalid day";
+        };
+         */
+
+    }
+    enum Days{
+        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+    }
+}
