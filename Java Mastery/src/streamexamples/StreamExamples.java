f51b2a9269bac2c66fe31a84510f470ec9c0110b
eljan_abdullazada <a3371977@gmail.com>
2026-03-30 00:37:48 +0400

    Session 3: Stream API lesson 23 Introduction to Stream

---
+++ Java Mastery/src/streamexamples/StreamExamples.java
@@ -0,0 +1,39 @@
+package streamexamples;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamExamples {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 4, 10, 3, 6, 1, 15, 4, 9, 16, 5);
+//        Stream<Integer> numStream = numbers.stream();
+//        numStream.forEach(System.out::println);
+        numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .map(n -> n + 1)
+                .sorted()
+                .distinct()
+                .forEach(System.out::println);
+
+        // Terminal operation Methods -> do not return a stream
+        // Intermediate operation Methods -> returns a stream
+
+        List<String> animals = new ArrayList<>();
+        animals.add("Cat");
+        animals.add("Dog");
+        animals.add("Cow");
+
+        Stream<String> animalStream = animals.stream();
+        animalStream.map(String::toLowerCase).forEach(System.out::println);
+//        animalStream.map(String::toUpperCase).forEach(System.out::println);
+//        since forEach is a terminal operation. The second operation cannot be run
+//        returning error like streams has already been operated upon or closed.
+
+
+
+
+
+    }
+}
